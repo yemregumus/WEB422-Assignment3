f2569e5ceb03970229a539b437fc0e82bcccd8d7
Yunus Gumus <ygumus@myseneaca.ca>
2023-10-14 21:23:30 -0400

    quotes escaped

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,9 +24,9 @@ export default function About({ movie }) {
         <Card className="bg-light">
           <Card.Body>
             <div>
-              <p>I'm currently enrolled in the Computer Programming and Analysis program at Seneca College, and my passion for technology and programming has been a driving force in my career choice. I've had the opportunity to work in various industries in the past, and while I have a broad skill set, I've decided to focus my expertise on becoming a programming expert. Throughout my career, I've prided myself on qualities such as leadership, teamwork, honesty, and integrity, which have consistently complemented my professional endeavors. Independence has been a significant aspect of my life since childhood, and I've always been self-reliant. I consider myself a self-made individual with a strong commitment to my education and a bright future in the field of technology.</p>
+              <p>I&apos;m currently enrolled in the Computer Programming and Analysis program at Seneca College, and my passion for technology and programming has been a driving force in my career choice. I&apos;ve had the opportunity to work in various industries in the past, and while I have a broad skill set, I&apos;ve decided to focus my expertise on becoming a programming expert. Throughout my career, I&apos;ve prided myself on qualities such as leadership, teamwork, honesty, and integrity, which have consistently complemented my professional endeavors. Independence has been a significant aspect of my life since childhood, and I&apos;ve always been self-reliant. I consider myself a self-made individual with a strong commitment to my education and a bright future in the field of technology.</p>
               <p>
-                It's difficult to choose a favourite, but "<Link href="/movies/The%20Matrix">{movie.title}</Link>" (released in 1999) is one of the most epic movies that I watch.
+                It&apos;s difficult to choose a favourite, but &quot;<Link href="/movies/The%20Matrix">{movie.title}</Link>&quot; (released in 1999) is one of the most epic movies that I watch.
               </p>
             </div>
           </Card.Body>
