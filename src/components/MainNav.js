94ad308d19d7db7b04a9bb3ab30a2e6c11de8e1b
Yunus Gumus <ygumus@myseneaca.ca>
2023-10-14 21:14:21 -0400

    some css added

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import Link from "next/link";
 
 function MainNav() {
+  const linkedInProfileUrl = "https://www.linkedin.com/in/jonasgumusyyz/";
+
   return (
     <>
       <Navbar fixed="top" expand="md" className="fixed-top bg-dark navbar-dark">
         <Container>
           <Nav fixed="top">
-            <Navbar.Brand>Yunus Gumus</Navbar.Brand>
+            <Navbar.Brand>
+              <a href="https://www.linkedin.com/in/jonasgumusyyz/" target="_blank" rel="noopener noreferrer" className="rainbow-link">
+                <span className="rainbow-text">Yunus Gumus</span>
+              </a>
+            </Navbar.Brand>
+
             <Link href="/" passHref legacyBehavior>
               <Nav.Link href="/">Movies</Nav.Link>
             </Link>
