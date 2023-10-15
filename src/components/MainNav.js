c84e8ed2d6b40e93519c9e223a56ce10420ecb5e
Yunus Gumus <ygumus@myseneaca.ca>
2023-10-15 07:18:43 -0400

    versions updated due dependency errors in netlify

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -3,19 +3,16 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import Link from "next/link";
 
 function MainNav() {
-  const linkedInProfileUrl = "https://www.linkedin.com/in/jonasgumusyyz/";
-
   return (
     <>
       <Navbar fixed="top" expand="md" className="fixed-top bg-dark navbar-dark">
         <Container>
           <Nav fixed="top">
             <Navbar.Brand>
-              <a href="https://www.linkedin.com/in/jonasgumusyyz/" target="_blank" rel="noopener noreferrer" className="rainbow-link">
+              <Link href="https://www.linkedin.com/in/jonasgumusyyz/" target="_blank" rel="noopener noreferrer" className="rainbow-link">
                 <span className="rainbow-text">Yunus Gumus</span>
-              </a>
+              </Link>
             </Navbar.Brand>
-
             <Link href="/" passHref legacyBehavior>
               <Nav.Link href="/">Movies</Nav.Link>
             </Link>
